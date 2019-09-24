20fb9b72a6750751290b390d7bfb6959dfbe5f2d
jorgefpont <jorgefpont@gmail.com>
2019-09-23 21:34:33 -0700

    homework 1

---
+++ hwk_1.py
@@ -0,0 +1,8 @@
+# This prints the result of multiplying 234*456
+print ("234 * 456 =")
+print (234*456)
+
+"""=================== RESTART: /home/jorge/csf021a/hwk_1.py ===================
+234 * 456 =
+106704
+"""
