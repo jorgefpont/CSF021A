d02e28344ddee9bde66090b11522755a9eefb3f1
jorgefpont <jorgefpont@gmail.com>
2019-11-21 21:26:44 -0800

    revised with changed init methods and eliminating setData methods

--- csf021aWork/employee.py
+++ csf021aWork/employee.py
@@ -3,15 +3,8 @@
 class Employee:
     """ One object of class Employee represents one Employee """
 
-    def __init__(self):
+    def __init__(self, first_name='', last_name='', ssn='', salary=0.0):
         """Employee constructor"""
-        self.first_name = ''
-        self.last_name = ''
-        self.ssn = ''
-        self.salary = 0.0
-
-    def setData(self, first_name, last_name, ssn, salary):
-        """Set emloyee data"""
         self.first_name = first_name
         self.last_name = last_name
         self.ssn = ssn
@@ -37,13 +30,13 @@ class Employee:
             return False
 
     def __lt__(self, other):
-      """
-      Returns True when the name in self is alphabetically less than
-      the name in other, and False otherwise.
-      If the last names are equal, checks the first names
-      to determine which object is less than the other.
-      Method is case insensitive
-      """
+        """
+        Returns True when the name in self is alphabetically less than
+        the name in other, and False otherwise.
+        If the last names are equal, checks the first names
+        to determine which object is less than the other.
+        Method is case insensitive
+        """
         # compare last names
         if self.last_name.lower() < other.last_name.lower():
             return True
@@ -54,10 +47,6 @@ class Employee:
             return True
 
         # if last names are equal, first name comparison case 2
-        elif self.last_name.lower() == other.last_name.lower() and \
-                self.first_name.lower() > other.first_name.lower():
-            return False
-
         # both first and last names are the same or
         # self.last_name > other.last_name
         else:
@@ -66,14 +55,10 @@ class Employee:
 
 if __name__ == "__main__":
     """Test the class"""
-    employee1 = Employee()
-    employee1.setData("Mickey", "Mouse", "123-12-1234", 100000)
-    employee2 = Employee()
-    employee2.setData("Minnie", "Mouse", "123-12-1235", 120000)
-    employee3 = Employee()
-    employee3.setData("Mickey", "Mouse", "555-12-1212", 100000)
-    employee4 = Employee()
-    employee4.setData("Mickey", "Smith", "555-12-1212", 100000)
+    employee1 = Employee("Mickey", "Mouse", "123-12-1234", 100000)
+    employee2 = Employee("Minnie", "Mouse", "123-12-1235", 120000)
+    employee3 = Employee("Mickey", "Mouse", "555-12-1212", 100000)
+    employee4 = Employee("Mickey", "Smith", "555-12-1212", 100000)
 
     print(employee1)
     print(employee2)
--- csf021aWork/manager.py
+++ csf021aWork/manager.py
@@ -7,33 +7,23 @@ class Manager (Employee):
     """ One object of class Manager represents one special class of
     employee who is also a manager """
 
-    def __init__(self):
+    def __init__(self, first_name='', last_name='', ssn='', salary='',
+                 title='', bonus=0.0):
+
         """initialize manager object"""
+        Employee.__init__(self, first_name, last_name, ssn, salary)
 
-        Employee.__init__(self)
-
-        self.title = ''
-        self.bonus = 0.0
+        self.title = title
+        self.bonus = bonus
 
     def __str__(self):
         """ print manager object"""
         return Employee.__str__(self) + "%s\n%.2f\n" % (self.title,
             self.bonus)
 
-    def setData(self, first_name, last_name, ssn, salary, title, bonus):
-        """Sets manager data"""
-        self.first_name = first_name
-        self.last_name = last_name
-        self.ssn = ssn
-        self.salary = salary
-        self.title = title
-        self.bonus = bonus
-
-
 if __name__ == "__main__":
     """ This is the test program to see how these two types of objects are constructed """
-    mgr1 = Manager()
-    mgr1.setData("Goofy", "Smith", "123-12-1236", 150000, "Director", 25000)
+    mgr1 = Manager("Goofy", "Smith", "123-12-1236", 150000, "Director", 25000)
     print (mgr1)
     mgr1.giveRaise(25)
     print(mgr1)
